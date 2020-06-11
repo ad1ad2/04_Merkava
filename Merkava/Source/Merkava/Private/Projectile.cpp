117d0585eb396e4fb3f0c618a6086ce302ff93dd
ad1ad2 <adcoolmail@gmail.com>
2020-06-11 19:25:41 +0300

    Setup Track Throttles

--- Merkava/Source/Merkava/Private/Projectile.cpp
+++ Merkava/Source/Merkava/Private/Projectile.cpp
@@ -29,8 +29,6 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::LaunchProjectile(float Speed)
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Projectile is Firing at %f"), Time, Speed);
     ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
